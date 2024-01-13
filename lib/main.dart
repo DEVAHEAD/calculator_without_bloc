d9fd9f59aca5de1c0e39374d32582570cfed6791
HeeyoungJoe <heeyoungjoe2000@gmail.com>
2024-01-13 15:02:28 +0900

    flatbutton included-deprecated

--- lib/main.dart
+++ lib/main.dart
@@ -73,3 +73,54 @@ class ResultDisplay extends StatelessWidget {
     );
   }
 }
+
+class CalculatorButton extends StatelessWidget {
+  CalculatorButton({
+    required this.label,
+    required this.onTap,
+    required this.size,
+    this.backgroundColor = Colors.white,
+    this.labelColor = Colors.black
+  });
+
+  final String label;
+  final VoidCallback onTap;
+  final double size;
+  final Color? backgroundColor;
+  final Color? labelColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(6),
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey, 
+              offset: Offset(1, 1), 
+              blurRadius: 2
+            ),
+          ],
+          borderRadius: BorderRadius.all(
+            Radius.circular(size / 2)
+          ),
+          color: backgroundColor
+        ),
+        child:FlatButton(
+          shape: CircleBorder(),
+          onPressed: onTap,
+          child: Center(
+            child: Text(
+              label,
+              style: TextStyle(fontSize: 24, color: labelColor),
+            )
+          ),
+        )// Label text and other stuff here
+      )
+    );
+  }
+
+}
