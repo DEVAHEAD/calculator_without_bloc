8f4ed465f93977da4799ab6d48c417ecc1502f74
HeeyoungJoe <heeyoungjoe2000@gmail.com>
2024-01-13 17:14:26 +0900

    interactivity

--- lib/main.dart
+++ lib/main.dart
@@ -26,13 +26,26 @@ class Calculation extends StatefulWidget {
 }
 
 class _CalculationState extends State<Calculation> {
-  late int result; //HY[240113] added late
+  
+  double width = 0;  
 
+  int? firstOperand;
+  String? operator;
+  int? secondOperand;
+  int? result;
+
+  @override
+  void didChangeDependencies() {
+    width = MediaQuery.of(context).size.width;
+    super.didChangeDependencies();
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        ResultDisplay(text: '0'),
+        ResultDisplay(
+          text: _getDisplayText(),
+        ),
         Row(
           children: [
             _getButton(text: '7', onTap: () => numberPressed(7)),
@@ -68,13 +81,108 @@ class _CalculationState extends State<Calculation> {
       ]
     );
   }
+
+  operatorPressed(String operator) {
+    setState(() {
+      if (firstOperand == null) {
+        firstOperand = 0;
+      }
+      this.operator = operator;
+    });
+  }
+
+  numberPressed(int number) {
+    setState(() {
+      if (result != null) {
+        result = null;
+        firstOperand = number;
+        return;
+      }
+      if (firstOperand == null) {
+        firstOperand = number;
+        return;
+      }
+      if (operator == null) {
+        firstOperand = int.parse('$firstOperand$number');
+        return;
+      }
+      if (secondOperand == null) {
+        secondOperand = number;
+        return;
+      }
+
+      secondOperand = int.parse('$secondOperand$number');
+    });
+  }
+
+  calculateResult() {
+    if (operator == null || secondOperand == null) {
+      return;
+    }
+    setState(() {
+      switch (operator) {
+        case '+':
+          result = firstOperand + secondOperand;
+          break;
+        case '-':
+          result = firstOperand - secondOperand;
+          break;
+        case '*':
+          result = firstOperand * secondOperand;
+          break;
+        case '/':
+          if (secondOperand == 0) {
+            return;
+          }
+          result = firstOperand ~/ secondOperand;
+          break;
+      }
+
+      firstOperand = result;
+      operator = null;
+      secondOperand = null;
+      result = null;
+    });
+  }
+
+  clear() {
+    setState(() {
+      result = null;
+      operator = null;
+      secondOperand = null;
+      firstOperand = null;
+    });
+  }
+  String _getDisplayText() {
+    if (result != null) {
+      return '$result';
+    }
+
+    if (secondOperand != null) {
+      return '$firstOperand$operator$secondOperand';
+    }
+
+    if (operator != null) {
+      return '$firstOperand$operator';
+    }
+
+    if (firstOperand != null) {
+      return '$firstOperand';
+    }
+
+    return '0';
+  }
+  
+  Widget _getButton({required String text, required VoidCallback onTap, Color backgroundColor = Colors.white, Color textColor = Colors.black}) { //HY [240113] added required
+  return CalculatorButton(
+    label: text,
+    onTap: onTap,
+    size: width / 4 - 12,
+    backgroundColor: backgroundColor,
+    labelColor: textColor,
+  );
 }
-
-operatorPressed(String operator) {}
-numberPressed(int number) {}
-calculateResult() {}
-clear() {}
-
+}
 
 
 class ResultDisplay extends StatelessWidget {
@@ -160,12 +268,5 @@ class CalculatorButton extends StatelessWidget {
 }
 
 
-Widget _getButton({required String text, required VoidCallback onTap, Color backgroundColor = Colors.white, Color textColor = Colors.black}) { //HY [240113] added required
-  return CalculatorButton(
-    label: text,
-    onTap: onTap,
-    size: 90,
-    backgroundColor: backgroundColor,
-    labelColor: textColor,
-  );
-}
+
+
